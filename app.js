86bb18884aa3daefa6094cc9cb867ee766f8008d
fedeval98 <fedeval0698@gmail.com>
2025-01-27 09:37:44 -0300

    implementando funcion y validacion agregarAmigo

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = []
 
+function capturarAmigo(){
+  let nuevoAmigo = document.getElementById("amigo").value
+  return nuevoAmigo
+}
+
+function entradaVacia(){
+  if(capturarAmigo() == null || capturarAmigo()== ""){
+    alert("Por favor, inserte un nombre")
+    return true
+  } else{
+    return false
+  }
+}
+
+function limpiarAmigo() {
+  let nuevoAmigo = document.getElementById("amigo")
+  nuevoAmigo.value = ""
+}
+
+function agregarAmigo () {
+  if (entradaVacia()){
+    return
+  }
+  amigos.push(capturarAmigo())
+  limpiarAmigo()
+}
 
